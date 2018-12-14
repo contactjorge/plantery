6e54c22b833d308cd85103303c63c740201c59c1
contactjorge <contactjorge@thejorgelopez.com>
2018-12-14 10:32:22 -0600

    Second Commit

---
+++ plantery.api.php
@@ -0,0 +1,12 @@
+<?php
+/**
+ * Implements hook_preprocess_html().
+ *
+ * Plantery theme
+ *
+ * Libraries for theme
+ * Jorge L Lopez - ChicagoConnect.Solutions
+ * Version 1811.14
+ * Copyright 2018
+ *
+ */
